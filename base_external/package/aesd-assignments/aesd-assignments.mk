4eb804cbc51e94fdebbd7972498581c874ac3f88
mukesh2006 <mukesh2006@gmail.com>
2024-12-16 13:33:55 +0400

    Update aesd-assignments.mk
    Resolving issue: start-stop-daemon: can't execute './aesdsocket': No such file or directory

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 0d31abadb0bcfcd362aee1883045ed4aba41c418
+AESD_ASSIGNMENTS_VERSION = 8a96acae87cf2c7e1d2d498b4ebd637e0feda413
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
